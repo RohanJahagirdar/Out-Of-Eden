ce046cd0e64dd59073a277522e78361a8e4594cf
RohanJahagirdar <rohan.s.jahagirdar@gmail.com>
2018-01-16 02:20:42 -0500

    CollapsingToolbarLayout added and working. Minor changes to UI.

--- app/src/main/java/me/rohanjahagirdar/outofeden/Chapter/ChapterActivity.kt
+++ app/src/main/java/me/rohanjahagirdar/outofeden/Chapter/ChapterActivity.kt
@@ -2,8 +2,10 @@ package me.rohanjahagirdar.outofeden.Chapter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import me.relex.circleindicator.CircleIndicator
 import me.rohanjahagirdar.outofeden.Onboarding.CustomPagerAdapter
 import me.rohanjahagirdar.outofeden.R
@@ -20,6 +22,13 @@ class ChapterActivity : AppCompatActivity() {
 
         val circleIndicator = findViewById<CircleIndicator>(R.id.indicator)
         circleIndicator.setViewPager(viewPager)
+
+        val mToolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(mToolbar)
+       // val mCollapsingToolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
+        //mCollapsingToolbarLayout.setTitleEnabled(false)
+
+        //mToolbar.setTitle("title")
 
     }
 }
